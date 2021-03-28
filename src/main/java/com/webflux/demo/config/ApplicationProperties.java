d6bf04905a92ac5881889ef123ccb3835bc2bcc4
madorik <madorik88@gmail.com>
2021-03-28 10:42:33 +0900

    data source 적용

--- src/main/java/com/webflux/demo/config/ApplicationProperties.java
+++ src/main/java/com/webflux/demo/config/ApplicationProperties.java
@@ -1,11 +1,13 @@
-package com.webflux.demo.pojo;
+package com.webflux.demo.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
+@PropertySource("classpath:/application.properties")
 public class ApplicationProperties {
 
     public static String PUSH_URL;
---
+++ src/main/java/com/webflux/demo/config/DataSourceConfig.java
@@ -0,0 +1,36 @@
+package com.webflux.demo.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@RequiredArgsConstructor
+@PropertySource("classpath:/hikari.properties")
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(dataSource());
+    }
+}
